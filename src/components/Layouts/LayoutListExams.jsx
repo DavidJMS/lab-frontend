bd0886d8822e02160df856524d932f892c13b7a5
MiguelJMS <miguelmarabay19@gmail.com>
2023-01-02 13:52:38 -0400

    Finalizando detalles

--- src/components/Layouts/LayoutListExams.jsx
+++ src/components/Layouts/LayoutListExams.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 import { Field } from '../shared/FormFields'
 import { getExams, deleteExams } from '../../services/exams'
 import { FormGroup } from '@mui/material'
-import { HStack, Box, Text, Input, Img, Table,Tr,Td,Tbody, useMediaQuery, Thead, Th } from '@chakra-ui/react'
+import { HStack, Box, Text, Button, Flex, Input, Img, Table,Tr,Td,Tbody, useToast, useMediaQuery, Thead, Th } from '@chakra-ui/react'
 import Header from './Header'
 import ModalTypesExams from '../modals/ModalTypesExams'
 import DeleteIcon from '../../assets/Delete.svg'
@@ -17,6 +17,7 @@ const LayoutListExams = () => {
   const [IsNotSmallScreen] = useMediaQuery('(min-width: 600px)')
   const title = 'Lista de examenes'
   const [dataExam, setDataExam] = useState([])
+  const toast = useToast()
   useEffect(() => {
     fetchUser()
   }, [])
@@ -36,25 +37,28 @@ const LayoutListExams = () => {
     try {
       setLoading(true)
       await deleteExams(id)
-      location.reload()
-      toast({
-        title: 'Exito',
-        description: 'Cliente eliminado de manera exitosa',
-        status: 'success',
-        duration: 3000,
-        isClosable: true,
-        position: 'top-right'
-      })
+      if (true) {
+        location.reload()
+       toast({
+         title: 'Exito',
+         description: 'Examen eliminado de manera exitosa',
+         status: 'success',
+         duration: 3000,
+         isClosable: true,
+         position: 'top-right'
+        })
+     } else {
+       toast({
+         title: 'Error',
+         description: 'Hubo en error, intentelo luego.',
+         status: 'error',
+         duration: 3000,
+         isClosable: true,
+         position: 'top-right'
+       })
+     }
     } catch (error) {
       setLoading(false)
-      toast({
-        title: 'Error',
-        description: 'Hubo en error, intentelo luego.',
-        status: 'error',
-        duration: 3000,
-        isClosable: true,
-        position: 'top-right'
-      })
     } finally {
       setLoading(false)
     }
@@ -73,10 +77,12 @@ const LayoutListExams = () => {
         <Text>
           Examen :
         </Text>
-
+        
         <Input w='30%' />
-        <ModalTypesExams />
+        <Button backgroundColor={"#D0D0D0"}>Buscar</Button>
+
       </HStack>
+   
       <Box width={'100%'} display={'flex'} flexDirection={'column'} alignItems={'center'}>
       <Table fontSize={['.8rem', '1rem']} variant='simple' width='90%' m={4}>
         {IsNotSmallScreen &&
@@ -99,6 +105,9 @@ const LayoutListExams = () => {
         </Tbody>
       </Table>
       </Box>
+      <Flex justifyContent={'flex-end'}>
+    <ModalTypesExams />
+    </Flex>
     </>
   )
 }
